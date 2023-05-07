fbef699030146a298947c5e235b7d3286b4a7f62
lakshmireddyct0017 <78538658+lakshmireddyct0017@users.noreply.github.com>
2023-05-07 12:00:41 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from "react-dom"
 import { App } from "./App"
 import "./index.css"
 import "bootstrap/dist/css/bootstrap.min.css"
+import {disableReactDevTools} from '@fvilers/disable-react-devtools';
+if(ProcessingInstruction.env.NODE_ENV==='production') disableReactDevTools()
 
 ReactDOM.render(<App />, document.getElementById("root"))
